764610ce52427dd162e5c4a2bc830b6b4993565c
Elozzy <st.eloghosa@gmail.com>
2020-03-04 17:41:49 -0800

    refactored the code base again

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,34 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import UserItem from './Useritem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ({ users, loading }) => {
-    if (loading) {
-      return <Spinner/>
-    }else {
+const Users = () => {
+  const githubContext = useContext(GithubContext);
 
-      return (
-        <div style={ userStyle }>
-            {users.map(user => (
-                <UserItem key={user.id} user={user} />   
-            ))}
-        </div>
+  const { loading, users } = githubContext;
+
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
     );
-    }
-
-  
-}
-
-Users.PropTypes = {
-  users: PropTypes.array.isRequired,
-  loading:PropTypes.bool.isRequired
-}
-
-const userStyle = {
-    display: "grid",
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
+  }
 };
 
-export default Users
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
+};
+
+export default Users;
